b7d39ad39eefd926c41ed4563ec3512357afd51e
Benjamin Rojas Barahona <136829212+benrojasbarahona@users.noreply.github.com>
2023-11-16 16:01:12 -0300

    m

--- archivos.py
+++ archivos.py
@@ -5,11 +5,7 @@ def lee_rodales(archivo_rodales:str) -> dict:
     # Lee rodales.csv y retorna un diccionario en donde la 
     # clave es el nombre del cliente y el contenido es una lista con la 
     # dirección, el teléfono y el monto adeudado, si no está, lo crea
-    DICC_RODALES = {}
-    with open(archivo_rodales,'r') as archivo_in:
-        
-    return DICC_RODALES
-
+    ...
 def escribe_rodales(file_clientes:str, DICC_RODALES: dict) -> bool:
     # Lee rodales.csv en el formato indicado y retorna True si salió
     # correctamente, este booleano se usa en la nube
