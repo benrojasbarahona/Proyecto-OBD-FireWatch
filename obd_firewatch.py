6666cda84be35801a4cff98e2deb98307a3ccb85
Tolally <m.toledovarela@gmail.com>
2023-11-20 19:20:30 -0300

    Ahora si funciona el .get()

--- obd_firewatch.py
+++ obd_firewatch.py
@@ -68,6 +68,10 @@ def main():
         ventana_ing_correcto = tk.Toplevel()
         ventana_ing_correcto.columnconfigure([0, 1, 2], minsize = 250, weight = 1)
         ventana_ing_correcto.rowconfigure([0, 1, 2], minsize = 250, weight = 1)
+        tk.Label(ventana_ingreso_correcto, text = "Su rodal ha sido ingresado correctamente !").grid(row=1,column=2,sticky="w")
+        boton_cerrar= ttk.Button(ventana_ingreso_correcto, text = "OK", command = ventana_ingreso_correcto.destroy)
+        boton_cerrar.grid(row=3,column=2)
+
 
 
     def ventana_ingresar():
@@ -94,63 +98,77 @@ def main():
         
         def boton_entrada_rodal():
             """Handler validaciones archivo"""
+            class ErrorEntradaVacia (Exception):
+                pass
+
             print("primero valido, despues retorno o tiro error")
             try:
-                id_rodal = str(entrada_rodal.get())
-                nativo = int(entrada_nativo.get())
-                exotico = int(entrada_exotico.get())
-                propietario = str(entrada_propietario.get())
-                
-            except AttributeError:
-                ventana_error_ingreso("Error, casillas vacias")
-                
-            datos_rodal = [id_rodal, nativo, exotico, propietario]
-            if validaciones_entradas(datos_rodal) == False:
-                ventana_error_ingreso()
+                id_rodal = entrada_rodal.get()
+                nativo = entrada_nativo.get()
+                exotico = entrada_exotico.get()
+                propietario = entrada_propietario.get() #llegar a diccionario
+                datos_rodal = [id_rodal, nativo, exotico, propietario]
+                for dato in datos_rodal:
+                    if dato == "":
+                        raise ErrorEntradaVacia("Error, alguna de las casillas esta vacia")
+                    
+            except ErrorEntradaVacia as msj:
+                ventana_error_ingreso(msj)
 
+            #ventana_ingreso_correcto()                
 
         #Entrada Rodal
         tk.Label(panel_izquierdo, text = "ID del Rodal").grid(row=1,column=1,sticky="w")
         tk.Label(panel_izquierdo, text = "(Ejemplo: R1)",fg="grey").grid(row=1,column=2,sticky="w")
-        entrada_rodal = tk.Entry(panel_izquierdo, width = 40, borderwidth = 2).grid(row=2,column=1,columnspan=3)
+        entrada_rodal = tk.Entry(panel_izquierdo, width = 40, borderwidth = 2)
+        entrada_rodal.grid(row=2,column=1,columnspan=3)
 
         #Entrada Bosque Nativo
         tk.Label(panel_izquierdo, text = "% Bosque Nativo").grid(row=4,column=1,sticky="w")
         tk.Label(panel_izquierdo, text = "(Ejemplo: 80)",fg="grey").grid(row=4,column=2,sticky="w")
-        entrada_nativo = tk.Entry(panel_izquierdo, width = 40, borderwidth = 2).grid(row=5,column=1,columnspan=3)
+        entrada_nativo = tk.Entry(panel_izquierdo, width = 40, borderwidth = 2, textvariable = int)
+        entrada_nativo.grid(row=5,column=1,columnspan=3)
 
         #Entrada Bosque Exótico
         tk.Label(panel_izquierdo, text = "% Bosque Exótico").grid(row=7,column=1,sticky="w")
         tk.Label(panel_izquierdo, text = "(Ejemplo: 20)",fg="grey").grid(row=7,column=2,sticky="w")
-        entrada_exotico = tk.Entry(panel_izquierdo, width = 40, borderwidth = 2).grid(row=8,column=1,columnspan=3)
+        entrada_exotico = tk.Entry(panel_izquierdo, width = 40, borderwidth = 2, textvariable = int)
+        entrada_exotico.grid(row=8,column=1,columnspan=3)
 
         #Entrada Propietario
         tk.Label(panel_izquierdo, text = "Nombre del Propietario").grid(row=10,column=1,sticky="w")
         tk.Label(panel_izquierdo, text = "(Ejemplo: Inv. Rojas)",fg="grey").grid(row=10,column=2,sticky="w")
-        entrada_propietario = tk.Entry(panel_izquierdo, width = 40, borderwidth = 2).grid(row=11,column=1,columnspan=3)
-
-        #Boton Añadir Rodal
-        boton_rodal = ttk.Button(panel_abajo, text = "Añadir Rodal", command=boton_entrada_rodal).grid(row=1,column=1)
+        entrada_propietario = tk.Entry(panel_izquierdo, width = 40, borderwidth = 2, textvariable = str)
+        entrada_propietario.grid(row=11,column=1,columnspan=3)
 
         tk.Label(panel_derecho, text = "Colindancias").grid(row=0,column=2,sticky="w")
 
         tk.Label(panel_derecho, text = "Norte").grid(row=1,column=1,sticky="w",padx=10)
-        entrada_norte = tk.Entry(panel_derecho, width = 40, borderwidth = 2).grid(row=2,column=1,columnspan=3,padx=10)
+        entrada_norte = tk.Entry(panel_derecho, width = 40, borderwidth = 2)
+        entrada_norte.grid(row=2,column=1,columnspan=3,padx=10)
 
         tk.Label(panel_derecho, text = "Noreste").grid(row=3,column=1,sticky="w",padx=10)
-        entrada_noreste = tk.Entry(panel_derecho, width = 40, borderwidth = 2).grid(row=4,column=1,columnspan=3)
+        entrada_noreste = tk.Entry(panel_derecho, width = 40, borderwidth = 2)
+        entrada_noreste.grid(row=4,column=1,columnspan=3)
 
         tk.Label(panel_derecho, text = "Noroeste").grid(row=5,column=1,sticky="w",padx=10)
-        entrada_noroeste = tk.Entry(panel_derecho, width = 40, borderwidth = 2).grid(row=6,column=1,columnspan=3)
+        entrada_noroeste = tk.Entry(panel_derecho, width = 40, borderwidth = 2)
+        entrada_noroeste.grid(row=6,column=1,columnspan=3)
 
         tk.Label(panel_derecho, text = "Sur").grid(row=7,column=1,sticky="w",padx=10)
-        entrada_sur = tk.Entry(panel_derecho, width = 40, borderwidth = 2).grid(row=8,column=1,columnspan=3)
+        entrada_sur = tk.Entry(panel_derecho, width = 40, borderwidth = 2)
+        entrada_sur.grid(row=8,column=1,columnspan=3)
 
         tk.Label(panel_derecho, text = "Sureste").grid(row=9,column=1,sticky="w",padx=10)
-        entrada_sureste = tk.Entry(panel_derecho, width = 40, borderwidth = 2).grid(row=10,column=1,columnspan=3)
+        entrada_sureste = tk.Entry(panel_derecho, width = 40, borderwidth = 2)
+        entrada_sureste.grid(row=10,column=1,columnspan=3)
 
         tk.Label(panel_derecho, text = "Suroeste").grid(row=11,column=1,sticky="w",padx=10)
-        entrada_suroeste = tk.Entry(panel_derecho, width = 40, borderwidth = 2).grid(row=12,column=1,columnspan=3,pady=10)
+        entrada_suroeste = tk.Entry(panel_derecho, width = 40, borderwidth = 2)
+        entrada_suroeste.grid(row=12,column=1,columnspan=3,pady=10)
+
+        #Boton Añadir Rodal
+        boton_rodal = ttk.Button(panel_abajo, text = "Añadir Rodal", command=boton_entrada_rodal).grid(row=1,column=1)
 
     def ventana_incendio():
         ventana_inc = tk.Toplevel() # crea ventana simulación incendio
